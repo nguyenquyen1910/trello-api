39fa3e651fe2eb31bb2b5c67fc3f04fb025fcad0
Nguyen Viet Quyen <quyennv@i.hivetech.vn>
2025-06-27 15:33:56 +0700

    feat: model design

---
+++ src/models/columnModel.js
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+import { OBJECT_ID_RULE, MONGO_OBJECT_ID_MESSAGE } from '../utils/validation.js';
+
+const COLUMN_COLLECTION_NAME = 'columns';
+const COLUMN_COLLECTION_SCHEMA = Joi.object({
+  boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(MONGO_OBJECT_ID_MESSAGE),
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  cardOrderIds: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false),
+});
+
+export const columnModel = {
+  COLUMN_COLLECTION_NAME, 
+  COLUMN_COLLECTION_SCHEMA,
+};
---
+++ src/utils/validators.js
@@ -0,0 +1,2 @@
+export const OBJECT_ID_RULE = /^[0-9a-fA-F]{24}$/;
+export const MONGO_OBJECT_ID_MESSAGE = 'Your string fails to match the required pattern';
